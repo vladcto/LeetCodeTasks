d8e3245ad007e93379ac01b6d5a42439471dda06
vladcto <55505918+vladcto@users.noreply.github.com>
2024-01-03 21:15:54 +0300

    refactor

--- 2.dart
+++ 2.dart
@@ -1,39 +1,25 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *   int val;
- *   ListNode? next;
- *   ListNode([this.val = 0, this.next]);
- * }
- */
 class Solution {
   ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
-    ListNode? nodeRemainder;
     if (l1 == null && l2 == null) {
       return null;
-    }
-    if (l1?.next != null) {
-      nodeRemainder = l1!.next;
-    } else if (l2?.next != null) {
-      nodeRemainder = l2!.next;
     }
 
     int value = (l1?.val ?? 0) + (l2?.val ?? 0);
     int remainder = value ~/ 10;
     value %= 10;
 
-    ListNode node = new ListNode();
-    node.val = value;
-    if (nodeRemainder == null) {
+    final remainNode = l1?.next ?? l2?.next;
+    final node = new ListNode(value);
+
+    if (remainNode == null) {
       if (remainder != 0) {
-        ListNode lastNode = ListNode();
-        lastNode.val = remainder;
-        node.next = lastNode;
+        node.next = ListNode(remainder);
       }
     } else {
-      nodeRemainder.val += remainder;
+      remainNode.val += remainder;
       node.next = addTwoNumbers(l1?.next, l2?.next);
     }
+
     return node;
   }
 }
@@ -41,5 +27,6 @@ class Solution {
 class ListNode {
   int val;
   ListNode? next;
+
   ListNode([this.val = 0, this.next]);
 }
