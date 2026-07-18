675661950e2f10ed815295b87e5d11e339713ebf
vladcto <55505918+vladcto@users.noreply.github.com>
2026-07-19 00:22:00 +0300

    cpp: 16

---
+++ 16.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <limits>
+#include <vector>
+
+class Solution {
+public:
+  int threeSumClosest(const std::vector<int> &nums, int target) {
+    auto closet = std::numeric_limits<int>::max();
+    auto closetSum = 0;
+
+    auto size = std::ranges::ssize(nums);
+    for (auto i = 0; i < size - 2; i++) {
+      for (auto j = i + 1; j < size - 1; j++) {
+        for (auto z = j + 1; z < size; z++) {
+          auto sum = nums[i] + nums[j] + nums[z];
+          auto diff = abs(target - sum);
+          if (diff < closet) {
+            closet = diff;
+            closetSum = sum;
+          }
+        }
+      }
+    }
+
+    return closetSum;
+  }
+};
