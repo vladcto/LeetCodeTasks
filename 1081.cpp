7adb71ff1ea82a22de0d9e070f7b6eadf2344e13
vladcto <55505918+vladcto@users.noreply.github.com>
2026-07-19 17:59:36 +0300

    cpp: 1081

---
+++ 1081.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+  std::string smallestSubsequence(std::string s) {
+    std::string result{};
+
+    std::unordered_set<char> uniqueLettersSet(s.begin(), s.end());
+    auto initialUniqueLettersCount = std::ranges::ssize(uniqueLettersSet);
+    auto lastLetterIt = s.rend();
+
+    for (auto i = 0; i < initialUniqueLettersCount; i++) {
+      std::unordered_set<char> uniqueLettersForSubsequenceSet{}, allowedChars{};
+	
+      for (auto it = s.rbegin(); it != lastLetterIt; it++) {
+        auto currentChar = *it;
+        // We've already added the char.
+        if (!uniqueLettersSet.contains(currentChar)) {
+          continue;
+        }
+        uniqueLettersForSubsequenceSet.insert(currentChar);
+        if (uniqueLettersSet.size() == uniqueLettersForSubsequenceSet.size()) {
+          allowedChars.insert(currentChar);
+        }
+      }
+
+      auto minChar = std::ranges::min(allowedChars);
+      result += minChar;
+      uniqueLettersSet.erase(minChar);
+      lastLetterIt = std::make_reverse_iterator(
+          std::find(lastLetterIt.base(), s.end(), minChar));
+    }
+
+    return result;
+  }
+};
